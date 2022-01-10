87c6f7b0e453e1606dda3a246c3390bfc6f0163e
Marcel Tencé <marcel.tence@u-psud.fr>
2022-01-10 15:09:23 +0100

    Do not create usim instrument if already there. orsay-instrumentation requires an instrument to be there.

--- nionswift_plugin/usim/__init__.py
+++ nionswift_plugin/usim/__init__.py
@@ -1,14 +1,27 @@
 from nion.utils import Registry
+from nion.instrumentation import HardwareSource
 
 from . import InstrumentDevice
 from . import CameraDevice
 from . import ScanDevice
 from . import InstrumentPanel
 
-def run():
-    instrument = InstrumentDevice.Instrument("usim_stem_controller")
-    Registry.register_component(instrument, {"instrument_controller", "stem_controller"})
+# Marcel Tencé
+# Instrument is already created by orsay-instrumentation exists
+# If not create instrument here.
+#
 
-    CameraDevice.run(instrument)
-    ScanDevice.run(instrument)
-    InstrumentPanel.run(instrument)
+def run():
+    #
+    # Swift works fine with only one instrument_controller!.
+    instrument = HardwareSource.HardwareSourceManager().get_instrument_by_id("usim_stem_controller")
+    if instrument is None:
+        instrument = InstrumentDevice.Instrument("usim_stem_controller")
+        Registry.register_component(instrument, {"instrument_controller", "stem_controller"})
+        CameraDevice.run(instrument)
+        ScanDevice.run(instrument)
+        InstrumentPanel.run(instrument)
+    if instrument is not None:
+        CameraDevice.run(instrument)
+        ScanDevice.run(instrument)
+        InstrumentPanel.run(instrument)
